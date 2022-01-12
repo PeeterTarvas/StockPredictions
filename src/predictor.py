a935cb0c72cd0c990bfb63d98e8457172302bf97
PeeterTarvas <peetertarvas@gmail.com>
2022-01-12 21:03:56 +0200

    added plot, it is a bit slow tho

--- src/predictor.py
+++ src/predictor.py
@@ -1,10 +1,15 @@
+import inline as inline
+import matplotlib
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
 import pandas as pd
 import sklearn.ensemble as sk
+import sklearn.pipeline as pipe
 import matplotlib.pyplot as plt
-from matplotlib.pylab import rcParams
+import sklearn.impute as impute
+import sklearn.model_selection as select
+
 
 
 class DataEngine:
@@ -12,35 +17,45 @@ class DataEngine:
     def __init__(self, csv_name: str, csv_col_names):
         self.frame = self.frame_data(csv_name).dropna(axis=0)
         self.columns = self.frame.columns
-        self.set_X_y_train(csv_col_names)
+        self.set_X_y(csv_col_names)
 
     def frame_data(self, csv_name: str):
         return pd.read_csv('/home/peeter/PycharmProjects/finData/resources/Data/Stocks/' + csv_name)
 
 
-    def set_X_y_train(self, csv_col_names):
-        y = self.frame['Open']
-        X = self.frame[csv_col_names]
-        self.train_X, self.val_X, self.train_y, self.val_y = train_test_split(X, y, random_state=0)
+    def set_X_y(self, csv_col_names):
+        self.y = self.frame['Open']
+        self.X = self.frame[csv_col_names]
 
-    def predict(self, leafs: int):
-        stock_model = sk.RandomForestRegressor(random_state=1, max_leaf_nodes=leafs)
-        stock_model.fit(self.train_X, self.train_y)
+    def create_model_as_pipeline(self, estimators):
+        stock_model = pipe.Pipeline(steps=[
+            ('processor', impute.SimpleImputer(strategy='mean')),
+            ('model', sk.RandomForestRegressor(random_state=0, n_estimators=estimators))
+        ])
         return stock_model
 
+    def test_different_CV(self, model: pipe.Pipeline, X, y, folds):
+        return select.cross_val_score(model, X, y, cv=folds).mean()
 
+    def predict(self, model: pipe.Pipeline, value):
+        return model.predict(value)
 
 if __name__ == '__main__':
-    features = ['Close', 'Volume']
+    features = ['Close', 'Volume', 'High', 'Low']
     engien = DataEngine('aapl.us.txt', features)
-    nodes_count = list(range(100, 10000, 100))
-    print(nodes_count)
+    nodes_count = list(range(100, 500, 100))
+    engien.set_X_y(features)
+    results = {}
     for i in nodes_count:
-        predictions = engien.predict(i).predict(engien.val_X)
-        print(mean_absolute_error(engien.val_y, predictions))
-        plt.scatter(engien.val_y[:100], predictions[:100], marker='o',  label='True')
-        # plt.plot(predictions, label='Predictions', color='red')
-        plt.show()
+        model = engien.create_model_as_pipeline(i)
+        test = engien.test_different_CV(model, engien.X, engien.y, 5)
+        results[i] = test
+        print(results)
+    plt.plot(list(results.keys()), list(results.values()))
+    plt.show()
+
+
+
 
 
 
